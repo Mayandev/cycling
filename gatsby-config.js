f8adbd474a259d5d2d623b4ce4eb07454525b60a
mayandev <phillzou@gmail.com>
2021-07-03 20:58:48 +0800

    feat: change npm register

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,7 @@ module.exports = {
     author: `@mayandev`,
   },
   plugins: [
+    'gatsby-plugin-postcss',
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sass`,
     `gatsby-plugin-image`,
